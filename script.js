36ad5f383107f6ddc7e24af74cb35e6cab70c63e
LeMineralee <155201315+LeMineralee@users.noreply.github.com>
2024-05-26 13:29:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let expression = '';
+
+function appendToResult(value) {
+  expression += value;
+  document.getElementById('result').value = expression;
+}
+
+function calculate() {
+  try {
+    const result = eval(expression);
+    if (result === 8) {
+      document.getElementById('result').value = result;
+    } else {
+      document.getElementById('result').value = 'i missss yoouuuuu';
+    }
+  } catch (error) {
+    document.getElementById('result').value = 'Error';
+  }
+}
+
+function clearResult() {
+  expression = '';
+  document.getElementById('result').value = '';
+}
